5753ef2256842962afa40fe7534d7c4218dda8cb
diogoheck <diogo.heck.dev@gmail.com>
2022-11-18 17:56:58 -0300

    Versao_1

--- app.py
+++ app.py
@@ -8,12 +8,28 @@ class Rota:
     fim_rateio = False
 
 
-def gerar_txt_saida(linha):
-    if linha[0] and linha[0] != 'Ev':
-        if len(linha) == 10:
-            print(f'{linha[0]} | {linha[2]} | {linha[4]} | {linha[5]} | {linha[7]} | {linha[9]}')
-        else:
-            print(linha)
+def gerar_txt_saida(linha, tabela_eventos, centro_de_custo):
+    
+    with open('log.txt', 'a') as log:
+        with open('layout_folha_importacao.txt', 'a', encoding='utf-8') as folha:
+            if linha[0] and linha[0] != 'Ev':
+                
+                # proventos e descontos
+                if len(linha) > 3:
+                    try:
+                        codigo_evento = int(linha[0])
+                        if tabela_eventos.get(codigo_evento):
+                            
+                            if tabela_eventos.get(codigo_evento).get(centro_de_custo, 'NAO ENCONTRADO'):
+                                
+                                print(f'conta debito {tabela_eventos.get(codigo_evento)[centro_de_custo]}', file=folha)
+                            elif tabela_eventos.get(codigo_evento).get(centro_de_custo, 'NAO ENCONTRADO') == 'NAO ENCONTRADO':
+                                print(f'centro de custos {centro_de_custo} nao encontrado ', file=log)    
+                        else:
+                            print(f'evento {codigo_evento} nao encontrado', file=log)
+
+                    except:
+                        pass
 
 
 def pegar_centro_custo(linha):
@@ -25,53 +41,69 @@ def pegar_centro_custo(linha):
         return linha[0].split('-')[-1].strip()
 
 
+
 def ler_tabela_eventos():
     dic_eventos = {}
     lista_custos = []
     cabecalho = True
     pasta_eventos = load_workbook('eventos.xlsx')
     planilha_eventos = pasta_eventos['planilha_eventos']
-    # lista_eventos = [linha for linha in planilha_eventos.values]
+    
     coluna = 4
     for linha in planilha_eventos.values:
         tam_colunas = len(linha)
-        if not cabecalho:
-            dic_eventos[linha[0]] = {'descricao': linha[1]}
-            dic_eventos[linha[0]].update({'hist': linha[2]}) 
-            dic_eventos[linha[0]].update({'credito': linha[3]}) 
-            if coluna < tam_colunas:
-                dic_eventos[linha[0]].update({lista_custos[coluna].get(coluna): linha[coluna]}) 
-                coluna += 1
-            # print(linha)
-        else:
+        if cabecalho:
+            lista_custos.append('')
+            lista_custos.append('')
+            lista_custos.append('')
+            lista_custos.append('')
             while(coluna < tam_colunas):
                 lista_custos.append({coluna:linha[coluna]})
                 coluna += 1
             coluna = 4
             cabecalho = False
-    print(lista_custos)
-
-    print(dic_eventos)
-    # return lista_eventos
+            
+        else:
+            coluna = 4
+            dic_eventos[linha[0]] = {'descricao': linha[1]}
+            dic_eventos[linha[0]].update({'hist': linha[2]}) 
+            dic_eventos[linha[0]].update({'credito': linha[3]}) 
+            while coluna < tam_colunas:
+                dic_eventos[linha[0]].update({lista_custos[coluna].get(coluna): linha[coluna]}) 
+                coluna += 1
+    
+    
+    return dic_eventos
 
 
 
 if __name__ == '__main__':
-    ler_tabela_eventos()
+    tabela_eventos = ler_tabela_eventos()
+    
+    if os.path.exists('layout_folha_importacao.txt'):
+        os.remove('layout_folha_importacao.txt')
 
+    if os.path.exists('log.txt'):
+        os.remove('log.txt')
 
+    centro_de_custo = None
     with open('Relatorios_Calculo_Relacao_de_Calculo_Rateada.csv') as folha:
+
         for linha in csv.reader(folha, delimiter=';'):
             if linha:
-                centro_de_custo = pegar_centro_custo(linha)
+                if 'Total do Rateio' in linha[0]:
+                    if linha[0].split('-')[-1].strip() == 'labores':
+                        centro_de_custo = 'Pro-labores'
+                    elif 'Sem Rateio' in linha[0]:
+                        centro_de_custo = None
+                    else:
+                        centro_de_custo = linha[0].split('-')[-1].strip()
                 if centro_de_custo:
                     Rota.centro_custo = True
-                    # print(centro_de_custo)
                 if 'Ev' in linha and Rota.centro_custo:
                     Rota.evento = True
                 if Rota.centro_custo and Rota.evento:
-                    # gerar_txt_saida(linha)
-                    pass
+                    gerar_txt_saida(linha, tabela_eventos, centro_de_custo)
                 if 'Parte RAT' in linha[0]:
                     Rota.centro_custo = False
                     Rota.evento = False
