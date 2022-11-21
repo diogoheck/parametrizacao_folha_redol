0a738e13afbf7b5197bac21b780e63753ebfe469
diogoheck <diogo.heck.dev@gmail.com>
2022-11-21 17:59:37 -0300

    Versao_2

--- app.py
+++ app.py
@@ -8,25 +8,64 @@ class Rota:
     fim_rateio = False
 
 
-def gerar_txt_saida(linha, tabela_eventos, centro_de_custo):
+def auxiliar_folha(codigo_evento, conta_debito, conta_credito, folha, descricao, valor):
+    if tabela_eventos.get(codigo_evento).get('tipo') == 'P':
+        print(f'{conta_debito}|{conta_credito}|{descricao}|{valor}|', file=folha)
+    else:
+        print(f'{conta_credito}|{conta_debito}{descricao}|{valor}|', file=folha)
+
+
+def layout_folha_sistema_redol(tabela_eventos, codigo_evento, folha, centro_de_custo, linha, provento):
+    conta_debito = tabela_eventos.get(codigo_evento)[centro_de_custo] 
+    conta_credito = tabela_eventos.get(codigo_evento)["credito"]
+    if provento:
+        valor = linha[4]
+        descricao = linha[2]
+    else:
+        valor = linha[9]
+        descricao = linha[7]
+    auxiliar_folha(codigo_evento, conta_debito, conta_credito, folha, descricao, valor)
+
+
     
+def lancar_folha(codigo_evento, log, tabela_eventos, centro_de_custo, linha, folha, provento):
+    if codigo_evento:
+        if tabela_eventos.get(codigo_evento):
+            
+            if tabela_eventos.get(codigo_evento).get(centro_de_custo, 'NAO ENCONTRADO'):
+                    layout_folha_sistema_redol(tabela_eventos, codigo_evento, folha, centro_de_custo, linha, provento)
+
+            elif tabela_eventos.get(codigo_evento).get(centro_de_custo, 'NAO ENCONTRADO') == 'NAO ENCONTRADO':
+                print(f'centro de custos {centro_de_custo} nao encontrado ', file=log)    
+        else:
+            print(f'evento {codigo_evento} nao encontrado', file=log)
+
+
+def gerar_txt_saida(linha, tabela_eventos, centro_de_custo):
+    provento = True
+
     with open('log.txt', 'a') as log:
         with open('layout_folha_importacao.txt', 'a', encoding='utf-8') as folha:
             if linha[0] and linha[0] != 'Ev':
                 
                 # proventos e descontos
-                if len(linha) > 3:
+                if len(linha) > 3 and len(linha) <= 10:
                     try:
                         codigo_evento = int(linha[0])
-                        if tabela_eventos.get(codigo_evento):
-                            
-                            if tabela_eventos.get(codigo_evento).get(centro_de_custo, 'NAO ENCONTRADO'):
-                                
-                                print(f'conta debito {tabela_eventos.get(codigo_evento)[centro_de_custo]}', file=folha)
-                            elif tabela_eventos.get(codigo_evento).get(centro_de_custo, 'NAO ENCONTRADO') == 'NAO ENCONTRADO':
-                                print(f'centro de custos {centro_de_custo} nao encontrado ', file=log)    
-                        else:
-                            print(f'evento {codigo_evento} nao encontrado', file=log)
+                        print(linha, len(linha))
+
+                        if len(linha) == 10:
+                            # lanca provento
+                            provento = True
+                            lancar_folha(codigo_evento, log, tabela_eventos, centro_de_custo, linha, folha, provento)
+                            # lanca desconto
+                            codigo_evento = linha[5]
+                            provento = False
+                            lancar_folha(codigo_evento, log, tabela_eventos, centro_de_custo, linha, folha, provento)
+                        elif len(linha) == 5:
+                            provento = True
+                            lancar_folha(codigo_evento, log, tabela_eventos, centro_de_custo, linha, folha, provento)
+                         
 
                     except:
                         pass
@@ -49,7 +88,7 @@ def ler_tabela_eventos():
     pasta_eventos = load_workbook('eventos.xlsx')
     planilha_eventos = pasta_eventos['planilha_eventos']
     
-    coluna = 4
+    coluna = 5
     for linha in planilha_eventos.values:
         tam_colunas = len(linha)
         if cabecalho:
@@ -57,17 +96,19 @@ def ler_tabela_eventos():
             lista_custos.append('')
             lista_custos.append('')
             lista_custos.append('')
+            lista_custos.append('')
             while(coluna < tam_colunas):
                 lista_custos.append({coluna:linha[coluna]})
                 coluna += 1
-            coluna = 4
+            coluna = 5
             cabecalho = False
             
         else:
-            coluna = 4
-            dic_eventos[linha[0]] = {'descricao': linha[1]}
-            dic_eventos[linha[0]].update({'hist': linha[2]}) 
-            dic_eventos[linha[0]].update({'credito': linha[3]}) 
+            coluna = 5
+            dic_eventos[linha[0]] = {'tipo': linha[1]}
+            dic_eventos[linha[0]].update({'descricao': linha[2]})
+            dic_eventos[linha[0]].update({'hist': linha[3]}) 
+            dic_eventos[linha[0]].update({'credito': linha[4]}) 
             while coluna < tam_colunas:
                 dic_eventos[linha[0]].update({lista_custos[coluna].get(coluna): linha[coluna]}) 
                 coluna += 1
@@ -79,6 +120,8 @@ def ler_tabela_eventos():
 
 if __name__ == '__main__':
     tabela_eventos = ler_tabela_eventos()
+
+    # print(tabela_eventos)
     
     if os.path.exists('layout_folha_importacao.txt'):
         os.remove('layout_folha_importacao.txt')
